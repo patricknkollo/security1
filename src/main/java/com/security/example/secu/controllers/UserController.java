fba1ecde7503e55dde571531eb86d09f85fddcd8
Ndedi Georges Patrick Nkollo (DHL DE), external <n.patrick_nkollo@deutschepost.de>
2023-10-18 21:55:31 +0200

    UsercontrollerTest

--- src/main/java/com/security/example/secu/controllers/UserController.java
+++ src/main/java/com/security/example/secu/controllers/UserController.java
@@ -6,6 +6,9 @@ import com.security.example.secu.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +19,7 @@ import java.security.Principal;
 import java.util.List;
 
 @Controller
+@ShellComponent
 public class UserController {
 
     private Logger logger = LoggerFactory.getLogger(UserController.class);
@@ -23,24 +27,28 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    @ShellMethod(key = "sec -ls", value = "get the list of all the users in the database")
     @RequestMapping(path = "/users", method = RequestMethod.GET)
     public @ResponseBody List<Users> getAllUserFromDB(){
         return service.findAllUser();
     }
 
+    @ShellMethod(key = "sec pull id", value = "get the list of all the users in the database")
     @RequestMapping(path = "/users/userid/id", method = RequestMethod.GET)
-    public @ResponseBody Users getUserById(@RequestParam("id") Long userid) throws NoUserFoundException {
+    public @ResponseBody Users getUserById(@ShellOption(value = "-i") @RequestParam("id") Long userid) throws NoUserFoundException {
         return service.findUserWithID(userid);
     }
 
+    @ShellMethod(key = "sec pull username", value = "get the list of all the users in the database")
     @RequestMapping(path = "/users/name/nom", method = RequestMethod.GET)
-    public @ResponseBody Users getUserByUsername(@RequestParam("username") String username, Principal principal) throws NoUserFoundException {
-        logger.info("the user is: {}", principal.getName());
+    public @ResponseBody Users getUserByUsername(@ShellOption(value = "-u")@RequestParam("username") String username) throws NoUserFoundException {
+        //logger.info("the user is: {}", principal.getName());
         return service.findUserWithUsername(username);
     }
 
+    @ShellMethod(key = "sec pull mail", value = "get the list of all the users in the database")
     @RequestMapping(path = "/users/email/mail", method = RequestMethod.GET)
-    public @ResponseBody Users getUserByUserMail(@RequestParam("email") String mail) throws NoUserFoundException {
+    public @ResponseBody Users getUserByUserMail(@ShellOption(value = "-e")@RequestParam("email") String mail) throws NoUserFoundException {
         return service.findUserWithEmail(mail);
     }
 }
---
+++ src/test/java/com/security/example/secu/UserControllerTest.java
@@ -0,0 +1,40 @@
+package com.security.example.secu;
+
+import com.security.example.secu.controllers.UserController;
+import com.security.example.secu.entities.Users;
+import com.security.example.secu.services.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+//@SpringBootTest  -->  more for integration Tests
+@ExtendWith(MockitoExtension.class)  // --> for pure Mocking
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UserControllerTest {
+
+
+    @InjectMocks
+    private UserController controller;
+
+    @Mock
+    private UserService service;
+
+    private Users user = new Users(1L, "username", "password", "email");
+
+    @Test
+    void test_getAllUserFromDB(){
+        List<Users> usersList = Arrays.asList(user);
+        Mockito.when(service.findAllUser()).thenReturn(usersList);
+        List<Users>result = controller.getAllUserFromDB();
+        Assertions.assertEquals(usersList, result);
+
+    }
+}
